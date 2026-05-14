75318927633a0bb9f68b5acdd3572b787ddc617b
trevanian662 <efebeydili@gmail.com>
2026-05-15 01:04:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "aetheria-v1";
+const urlsToCache = [
+    "index.html",
+    "style.css",
+    "game.js",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+        })
+    );
+});
